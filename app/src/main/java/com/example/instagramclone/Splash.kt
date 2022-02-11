6c529c0976f47b2f0e22e75edc07514fe8705547
IamBansal <abansal11ae@gmail.com>
2022-02-11 21:34:36 +0530

    UI Changed

--- app/src/main/java/com/example/instagramclone/Splash.kt
+++ app/src/main/java/com/example/instagramclone/Splash.kt
@@ -1,14 +1,34 @@
 package com.example.instagramclone
 
 import android.content.Intent
+import android.content.res.Configuration
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.widget.ImageView
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val image = findViewById<ImageView>(R.id.logoSplash)
+        val imageD = findViewById<ImageView>(R.id.logoSplashDark)
+
+        when (this.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                imageD.visibility = View.VISIBLE
+                image.visibility = View.GONE
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                image.visibility = View.VISIBLE
+                imageD.visibility = View.GONE
+            }
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+            }
+        }
 
         Handler().postDelayed({ startActivity(Intent(this, Login::class.java))}, 2500)
 
